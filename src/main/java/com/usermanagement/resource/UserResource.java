e09c1b96b48f64223c8ebd48139f32c2bc90f106
BURKAN AKYUREK <burkanakyurek@hotmail.com>
2022-01-04 11:52:19 +0300

    Create newUser method in userResource

--- src/main/java/com/usermanagement/resource/UserResource.java
+++ src/main/java/com/usermanagement/resource/UserResource.java
@@ -15,8 +15,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.mail.MessagingException;
+
+import java.io.IOException;
 
 import static com.usermanagement.constant.SecurityConstant.*;
 import static org.springframework.http.HttpStatus.*;
@@ -45,13 +48,26 @@ public class UserResource extends ExceptionHandling {
         return new ResponseEntity<>(loginUser, jwtHeader, OK);
     }
 
-
     @PostMapping("/register")
     public ResponseEntity<User> register(@RequestBody User user) throws UserNotFoundExcepiton, EmailExistException, UsernameExistException, MessagingException {
        User newUser = userService.register(user.getFirstName(),user.getLastName(),user.getUsername(),user.getEmail());
        return new ResponseEntity<>(newUser, OK);
     }
 
+    @PostMapping("/add")
+    public ResponseEntity<User> addNewUser(@RequestParam("firstname") String firstname,
+                                           @RequestParam("lastname") String lastname,
+                                           @RequestParam("username") String username,
+                                           @RequestParam("email") String email,
+                                           @RequestParam("role") String role,
+                                           @RequestParam("isActive") String isActive,
+                                           @RequestParam("isNoneLocked") String isNoneLocked,
+                                           @RequestParam(value = "profileImage", required = false) MultipartFile profileImage) throws EmailExistException, UserNotFoundExcepiton, IOException, UsernameExistException {
+
+        User newUser = userService.addNewUser(firstname, lastname, username, email, role, Boolean.parseBoolean(isActive), Boolean.parseBoolean(isNoneLocked), profileImage);
+
+        return new ResponseEntity<>(newUser, OK);
+    }
 
     private HttpHeaders getJwtHeader(UserPrincipal user) {
         HttpHeaders headers = new HttpHeaders();
